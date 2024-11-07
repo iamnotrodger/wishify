8e91836fa26fb3af5abc98c4db05bc68396fb550
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-11-07 08:10:30 -0500

    chore(ui): import tailwind and global css config to extension and web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from '@repo/ui/button';
-import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <Button>Test</Button>
     </div>
   );
