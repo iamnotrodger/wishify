03de01c74c6f05557c1f01f51fd8614ff132b341
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-11-11 11:42:25 -0500

    feat(scraper): implement ProductScraper `getMetadata` method

--- packages/scraper/src/scraper.ts
+++ packages/scraper/src/scraper.ts
@@ -25,6 +25,58 @@ export default class ProductScraper implements Scraper {
   }
 
   getMetadata(): Record<string, string> | null {
-    throw new Error('Method not implemented.');
+    const titleSelectors = [
+      'meta[property="og:title"]',
+      'meta[name="twitter:title"]',
+    ].join(',');
+    const title =
+      this.$(titleSelectors).attr('content') || this.$('title').text();
+
+    const descriptionSelectors = [
+      'meta[property="og:description"]',
+      'meta[name="twitter:description"]',
+      'meta[name="description"]',
+    ].join(',');
+    const description = this.$(descriptionSelectors).attr('content');
+
+    const url =
+      this.$('meta[property="og:url"]').attr('content') ||
+      this.$('link[rel="canonical"]').attr('href');
+
+    const imageSelectors = [
+      'meta[property="og:image:secure_url"]',
+      'meta[property="og:image:url"]',
+      'meta[property="og:image"]',
+      'meta[name="twitter:image"]',
+    ].join(',');
+    const image = this.$(imageSelectors).attr('content');
+
+    const priceSelectors = [
+      'meta[property="og:image:secure_url"]',
+      'meta[property="og:image:url"]',
+      'meta[property="og:image"]',
+      'meta[name="twitter:image"]',
+    ].join(',');
+    const price = this.$(priceSelectors).attr('content');
+
+    const currency = this.$('meta[property="product:price:currency"]').attr(
+      'content'
+    );
+
+    const favicon =
+      this.$('link[rel*="icon" i]').attr('href') ||
+      this.$('meta[name*="msapplication"]').attr('content');
+
+    return Object.fromEntries(
+      Object.entries({
+        title,
+        description,
+        url,
+        image,
+        price,
+        currency,
+        favicon,
+      }).filter(([_, value]) => value != null)
+    ) as Record<string, string>;
   }
 }
