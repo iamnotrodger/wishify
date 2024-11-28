4c47de0d319e366e1e1d698b898cfc69208324c5
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-11-27 20:52:23 -0500

    fix(web): add basic components/styling in home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from '@repo/ui/components/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Test</Button>
-    </div>
+    <main>
+      <section className='mx-auto my-6 flex h-[80vh] max-w-md flex-col items-center justify-center gap-2'>
+        <h1 className='text-5xl font-bold text-primary'>Wishify</h1>
+        <div className='my-2.5 text-center'>
+          <h2 className='text-xl font-semibold'>
+            Turn your wishlist into a plan
+          </h2>
+          <h3 className='text-primary'>name tbd</h3>
+        </div>
+        <Link href='/app'>
+          <Button>Go to app</Button>
+        </Link>
+      </section>
+    </main>
   );
 }
