374aed228d7e99544e84e6a5585b120d9565c54f
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-12-01 18:43:23 -0500

    feat(web): update home page, display ProductCard and add buttons

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,3 +1,26 @@
+import { ProductCard } from '@/components/product-card';
+import { Button } from '@repo/ui/components/button';
+import { SidebarTrigger } from '@repo/ui/components/sidebar';
+import { Plus } from 'lucide-react';
+
 export default function Home() {
-  return <div className=''></div>;
+  return (
+    <div className='p-4 md:px-20 md:py-10'>
+      <header className='flex shrink-0 items-center gap-2'>
+        <SidebarTrigger className='' />
+        <h1 className='text-2xl font-medium tracking-tight'>Inbox</h1>
+        <Button className='ml-auto'>
+          <Plus />
+          Add new item
+        </Button>
+      </header>
+      <main className='py-4'>
+        <div className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4'>
+          {Array.from({ length: 20 }).map((_, i) => (
+            <ProductCard key={i} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
