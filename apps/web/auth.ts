27d53e54571317c89631e6343516681d3ca20647
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-12-01 19:04:49 -0500

    feat(web): create default categories on user creation

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -1,7 +1,8 @@
+import authConfig from '@/auth.config';
+import { safeAsync } from '@/lib/utils';
 import { prisma } from '@/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth, { Session } from 'next-auth';
-import authConfig from '@/auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -16,6 +17,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     session({ session, token }) {
       session.user.id = token.id as string;
       return session;
+    },
+  },
+  events: {
+    createUser: async (message) => {
+      const { user } = message;
+      const [, error] = await safeAsync(
+        prisma.category.createMany({
+          data: [
+            { name: 'Clothes', icon: 'shirt', userId: user.id! },
+            { name: 'Electronics', icon: 'smartphone', userId: user.id! },
+            { name: 'Furniture', icon: 'sofa', userId: user.id! },
+          ],
+        })
+      );
+
+      if (error) console.log(error);
     },
   },
   ...authConfig,
