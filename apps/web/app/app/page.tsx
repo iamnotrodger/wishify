64f8e1e25963ee2fa590566dcda716686e49ef12
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-12-04 19:38:59 -0500

    feat(web): implement authentication check and fetch products in app page

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,9 +1,24 @@
+import { auth, isAuthenticated } from '@/auth';
 import { ProductCard } from '@/components/product-card';
+import { getProducts } from '@/services/product-service';
 import { Button } from '@repo/ui/components/button';
 import { SidebarTrigger } from '@repo/ui/components/sidebar';
 import { Plus } from 'lucide-react';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function App() {
+  const session = await auth();
+  if (!isAuthenticated(session)) {
+    return redirect('/login?redirect_to=/app');
+  }
+
+  const [products, error] = await getProducts(
+    { sort_by: 'createdAt', sort_dir: 'desc', limit: 50 },
+    session
+  );
+
+  if (error) console.log(error);
+
   return (
     <div className='p-4 md:px-20 md:py-10'>
       <header className='flex shrink-0 items-center gap-2'>
@@ -16,8 +31,8 @@ export default function Home() {
       </header>
       <main className='py-4'>
         <div className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4'>
-          {Array.from({ length: 20 }).map((_, i) => (
-            <ProductCard key={i} />
+          {products?.map((product) => (
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </main>
