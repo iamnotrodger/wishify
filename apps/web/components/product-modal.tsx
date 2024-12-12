5a4ed6dbca305c6624353e5843e9e697cafc1f4d
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2024-12-12 17:30:09 -0500

    fix(web): include image in product-modal component

--- apps/web/components/product-modal.tsx
+++ apps/web/components/product-modal.tsx
@@ -7,13 +7,14 @@ import {
   Dialog,
   DialogContent,
   DialogFooter,
-  DialogHeader,
-  DialogTitle,
 } from '@repo/ui/components/dialog';
 import { Input } from '@repo/ui/components/input';
 import { Label } from '@repo/ui/components/label';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+
+const PLACEHOLDER_IMAGE =
+  'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png';
 
 export function ProductModal() {
   const [open, setOpen] = useState(false);
@@ -48,45 +49,66 @@ export function ProductModal() {
 
   return (
     <Dialog open={open} onOpenChange={handleClose}>
-      <DialogContent className='sm:max-w-[425px]'>
-        <DialogHeader>
-          <DialogTitle>Update Product</DialogTitle>
-        </DialogHeader>
-        <div className='grid gap-4 py-4'>
-          <div className='grid grid-cols-8 items-center gap-4'>
-            <Label htmlFor='url' className='text-right'>
-              URL
-            </Label>
-            <Input
-              id='url'
-              className='col-span-7'
-              value={product?.url ?? undefined}
+      <DialogContent className='overflow-hidden p-0 sm:max-w-[750px]'>
+        <div className='flex'>
+          <div className='flex-1'>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className='h-full w-full object-cover transition-all duration-300'
+              src={
+                product && product.images && product.images[0]
+                  ? product.images[0]?.url
+                  : PLACEHOLDER_IMAGE
+              }
+              alt={`${product?.brand} - ${product?.name}`}
+              loading='lazy'
+              onError={(e) => {
+                e.currentTarget.src = PLACEHOLDER_IMAGE;
+              }}
             />
           </div>
-          <div className='grid grid-cols-8 items-center gap-4'>
-            <Label htmlFor='name' className='text-right'>
-              Name
-            </Label>
-            <Input
-              id='name'
-              className='col-span-7'
-              value={product?.name ?? undefined}
-            />
-          </div>
-          <div className='grid grid-cols-8 items-center gap-4'>
-            <Label htmlFor='price' className='text-right'>
-              Price
-            </Label>
-            <Input
-              id='price'
-              className='col-span-7'
-              value={product?.price ?? undefined}
-            />
+          <div className='mx-10 my-14 h-96 flex-1'>
+            <div className='grid gap-4 py-4'>
+              <div className='grid grid-cols-8 items-center gap-4'>
+                <Label htmlFor='name' className='text-right'>
+                  Name
+                </Label>
+                <Input
+                  id='name'
+                  className='col-span-7'
+                  value={product?.name ?? undefined}
+                  readOnly
+                />
+              </div>
+              <div className='grid grid-cols-8 items-center gap-4'>
+                <Label htmlFor='price' className='text-right'>
+                  Price
+                </Label>
+                <Input
+                  id='price'
+                  className='col-span-7'
+                  value={product?.price ?? undefined}
+                  readOnly
+                />
+              </div>
+              <div className='grid grid-cols-8 items-center gap-4'>
+                <Label htmlFor='url' className='text-right'>
+                  URL
+                </Label>
+                <Input
+                  id='url'
+                  className='col-span-7'
+                  value={product?.url ?? undefined}
+                  readOnly
+                />
+              </div>
+            </div>
+
+            <DialogFooter className='mt-auto'>
+              <Button type='submit'>Save</Button>
+            </DialogFooter>
           </div>
         </div>
-        <DialogFooter>
-          <Button type='submit'>Update</Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
