aabd023a6a441a0637032f0391f9d6bd2c94fb37
rodger.retanal24@gmail.com <rodger.retanal24@gmail.com>
2025-02-16 17:12:42 -0500

    feat(web): add product-list component with infinite scroll

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,11 +1,16 @@
 import { auth, isAuthenticated } from '@/auth';
-import { ProductCard } from '@/components/product-card';
 import { ProductForm } from '@/components/product-form';
-import { getProducts } from '@/services/product-service';
 import { Button } from '@repo/ui/components/button';
 import { SidebarTrigger } from '@repo/ui/components/sidebar';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 import { Plus } from 'lucide-react';
 import { redirect } from 'next/navigation';
+import { getProductsActions } from '@/app/actions';
+import ProductList from '@/components/product-list';
 
 export default async function App() {
   const session = await auth();
@@ -13,32 +18,38 @@ export default async function App() {
     return redirect('/login?redirectTo=/app');
   }
 
-  const [products, error] = await getProducts(
-    { sortBy: 'createdAt', sortDir: 'desc', limit: 50 },
-    session
-  );
-
-  if (error) throw error;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ['posts'],
+    initialPageParam: undefined,
+    queryFn: async () => {
+      const [products, error] = await getProductsActions({
+        sortBy: 'createdAt',
+        sortDir: 'desc',
+        limit: 50,
+      });
+      if (error) throw error;
+      return products;
+    },
+  });
 
   return (
-    <div className='p-4 md:px-20 md:py-10'>
-      <header className='flex shrink-0 items-center gap-2'>
-        <SidebarTrigger className='text-foreground h-8 w-8' />
-        <h1 className='text-2xl font-medium tracking-tight'>Inbox</h1>
-        <ProductForm>
-          <Button className='ml-auto'>
-            <Plus />
-            Add new item
-          </Button>
-        </ProductForm>
-      </header>
-      <main className='py-4'>
-        <div className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4'>
-          {products?.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </main>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className='p-4 md:px-20 md:py-10'>
+        <header className='flex shrink-0 items-center gap-2'>
+          <SidebarTrigger className='text-foreground h-8 w-8' />
+          <h1 className='text-2xl font-medium tracking-tight'>Inbox</h1>
+          <ProductForm>
+            <Button className='ml-auto'>
+              <Plus />
+              Add new item
+            </Button>
+          </ProductForm>
+        </header>
+        <main className='py-4'>
+          <ProductList />
+        </main>
+      </div>
+    </HydrationBoundary>
   );
 }
---
+++ apps/web/components/product-list.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { ProductCard } from '@/components/product-card';
+import InfiniteScroll from '@repo/ui/components/infinite-scroll';
+import { cn } from '@repo/ui/lib/utils';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { Loader2 } from 'lucide-react';
+import { Fragment } from 'react';
+import { getProductsActions } from '../app/actions';
+
+const FETCH_LIMIT = 50;
+
+const fetchProducts = async ({ pageParam }: { pageParam?: string }) => {
+  const [products, error] = await getProductsActions({
+    sortBy: 'createdAt',
+    sortDir: 'desc',
+    cursor: pageParam,
+    limit: FETCH_LIMIT,
+  });
+  if (error) throw error;
+  return products;
+};
+
+export default function ProductList() {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['posts'],
+      initialPageParam: undefined,
+      queryFn: fetchProducts,
+      getNextPageParam: (lastPage) => {
+        if (!lastPage || lastPage.length < FETCH_LIMIT) return undefined;
+        return lastPage[lastPage.length - 1]?.id;
+      },
+    });
+
+  return (
+    <div>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4'>
+        {data?.pages.map((page, i) => (
+          <Fragment key={i}>
+            {page?.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+
+      <InfiniteScroll
+        hasMore={hasNextPage}
+        isLoading={isFetchingNextPage}
+        next={fetchNextPage}
+        threshold={1}
+      >
+        <div className='my-2 flex justify-center'>
+          <Loader2
+            className={cn(
+              'h-8 w-8 animate-spin opacity-0 transition',
+              isFetchingNextPage && hasNextPage && 'opacity-100'
+            )}
+          />
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+}
